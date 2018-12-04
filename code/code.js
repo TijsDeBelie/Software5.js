a84076f5e066487f98328fad8fe4564ee48da39a
AEnterprise <aenterprise2@gmail.com>
2018-12-04 17:25:49 +0100

    🗑

--- code/code.js
+++ code/code.js
@@ -1,17 +1,3 @@
-function isItFriday(today) {
-  if (today === "Friday") {
-    return "TGIF";
-  } else {
-    return "Nope";
-  }
-}
-
-function addProduct(name, quantity) {
-  var list = []
-  for (var i = 0; i < quantity; i++) list.push(name);
-  return list
-}
-
 function countInArray(array, what) {
   return array.filter(item => item == what).length;
 }
@@ -54,11 +40,7 @@ return await promise;
 
 
 module.exports = {
-  isItFriday: isItFriday,
-  addProduct: addProduct,
-  countInArray: countInArray,
   getDesigns: getDesigns,
   post: post
 
-}
-
+};
